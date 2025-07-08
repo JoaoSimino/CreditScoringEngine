8aed44f785163d94d2a4e2912ccf20d4879a6196
JoaoSimino <joaovsmathiasdev@hotmail.com>
2025-07-08 10:34:28 -0300

    Criacao dos endpoints de Cliente

---
+++ CreditScoringEngine.Domain/Entities/ClienteExtensions.cs
@@ -0,0 +1,27 @@
+﻿using CreditScoringEngine.Domain.DTOs;
+
+namespace CreditScoringEngine.Domain.Entities;
+
+public static class ClienteExtensions
+{
+    public static Cliente DtoToEntity(this Cliente cliente,ClienteDto clienteDto) 
+    {
+        cliente.Id = Guid.NewGuid();
+        cliente.Nome = clienteDto.Nome;
+        cliente.RendaMensal = clienteDto.RendaMensal;
+        cliente.Idade = clienteDto.Idade;
+        cliente.HistoricoCreditoSimulado = clienteDto.HistoricoCreditoSimulado;
+
+        return cliente;
+    }
+
+    public static Cliente Update(this Cliente cliente, ClienteDto clienteDto) 
+    {
+        cliente.Nome = clienteDto.Nome;
+        cliente.RendaMensal = clienteDto.RendaMensal;
+        cliente.Idade = clienteDto.Idade;
+        cliente.HistoricoCreditoSimulado = clienteDto.HistoricoCreditoSimulado;
+
+        return cliente;
+    }
+}
