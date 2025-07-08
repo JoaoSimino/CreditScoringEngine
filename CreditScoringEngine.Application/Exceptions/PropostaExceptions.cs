2892bfde16c94b54da4fac2594620f7781bdffc1
JoaoSimino <joaovsmathiasdev@hotmail.com>
2025-07-08 09:33:01 -0300

    Criacao de CRUD de proposta

---
+++ CreditScoringEngine.Application/Exceptions/PropostaExceptions.cs
@@ -0,0 +1,10 @@
+﻿namespace CreditScoringEngine.Application.Exceptions;
+
+public class PropostaExceptions : Exception
+{
+    public PropostaExceptions(string? mensagem) : base(mensagem)
+    {
+
+    }
+
+}
---
+++ CreditScoringEngine.Application/Services/IPropostaService.cs
@@ -0,0 +1,7 @@
+﻿using CreditScoringEngine.Domain.Entities;
+
+namespace CreditScoringEngine.Application.Services;
+
+public interface IPropostaService : ICrudService<PropostaCredito>
+{
+}
