a92094023637042f034985aaebd2569abb2265cb
JoaoSimino <joaovsmathiasdev@hotmail.com>
2025-07-16 09:43:22 -0300

    atualizacao do program.cs para permitir que os servicos sejam configrados e a migration rodada automaticamente quando necessario ao se conectar no contanier Docker

--- CreditScoringEngine.API/Docker/script.sh
+++ CreditScoringEngine.API/Docker/script.sh
@@ -1,4 +1,4 @@
 # container de mssql para substituir o banco ate entao rodando local
 #comando para rodar meu container passando a string de conexao correta!
-#docker run -e "ConnectionStrings__DefaultConnection=Server=host.docker.internal,1433;Database=CreditScoringEngine;User Id=sa;Password=MyStr0ng!Passw0rd;TrustServerCertificate=True;" -p 8080:80 minimalapikickoff
+#docker run --rm -e "ConnectionStrings__DefaultConnection=Server=host.docker.internal,1433;Database=CreditScoringEngine;User Id=sa;Password=MyStr0ng!Passw0rd;TrustServerCertificate=True;" -p 8080:80 minimalapikickoff
 docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=MyStr0ng!Passw0rd" -p 1433:1433 --name sqlserver-container -d mcr.microsoft.com/mssql/server:2022-latest
--- CreditScoringEngine.API/Program.cs
+++ CreditScoringEngine.API/Program.cs
@@ -10,10 +10,19 @@ using System.Text.Json.Serialization;
 using Hangfire;
 using Hangfire.MemoryStorage;
 using CreditScoringEngine.Infrastructure.Messaging;
+using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configura logger mínimo para permitir logs antes do banco existir
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .CreateLogger();
 
+builder.Host.UseSerilog();
+
+// Ambiente de testes usa InMemory
 if (builder.Environment.IsEnvironment("Test"))
 {
     builder.Services.AddDbContext<CreditScoringEngineContext>(options =>
@@ -23,6 +32,73 @@ else
 {
     builder.Services.AddDbContext<CreditScoringEngineContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+}
+
+builder.Services.AddScoped<IClienteService, ClienteService>();
+builder.Services.AddScoped<IPropostaService, PropostaService>();
+builder.Services.AddScoped<IPropostaProcessingService, PropostaProcessingService>();
+builder.Services.AddSingleton<IMessagePublisher, RabbitMQPublisher>();
+
+builder.Services.AddProblemDetails();
+builder.Services.AddExceptionHandler<ClienteExceptionHandler>();
+builder.Services.AddExceptionHandler<ClienteNotFoundExceptionHandler>();
+builder.Services.AddExceptionHandler<PropostaExceptionHandler>();
+builder.Services.AddExceptionHandler<PropostaNotFoundExceptionsHandler>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureHttpJsonOptions(options => {
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+builder.Services.AddHangfire(config =>
+{
+    config.UseMemoryStorage();
+});
+builder.Services.AddHangfireServer();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermissiveCors", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// --- CRIA BANCO SE NÃO EXISTIR ---
+if (!builder.Environment.IsEnvironment("Test"))
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var config = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+        var fullConnectionString = config.GetConnectionString("DefaultConnection");
+
+        var builderWithoutDb = new SqlConnectionStringBuilder(fullConnectionString)
+        {
+            InitialCatalog = "master"
+        };
+
+        var databaseName = new SqlConnectionStringBuilder(fullConnectionString).InitialCatalog;
+
+        using var connection = new SqlConnection(builderWithoutDb.ConnectionString);
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = $@"
+            IF DB_ID(N'{databaseName}') IS NULL
+            BEGIN
+                CREATE DATABASE [{databaseName}];
+            END";
+
+        command.ExecuteNonQuery();
+    }
+
+    // --- RECONFIGURA O SERILOG AGORA COM SINK NO BANCO, QUE JÁ ESTÁ CRIADO ---
     var outputTemplate = "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}";
     var sinkOptions = new MSSqlServerSinkOptions
     {
@@ -30,7 +106,6 @@ else
         AutoCreateSqlTable = true,
     };
 
-    //column configurations
     var columnOptions = new ColumnOptions();
     columnOptions.Store.Remove(StandardColumn.Properties);
     columnOptions.Store.Add(StandardColumn.LogEvent);
@@ -51,52 +126,21 @@ else
             restrictedToMinimumLevel: LogEventLevel.Information
         )
         .WriteTo.File(
-        path: "Logs/app.log",
-        outputTemplate: outputTemplate,
-        rollingInterval: RollingInterval.Day,
-        flushToDiskInterval: TimeSpan.FromSeconds(1),
-        shared: true
+            path: "Logs/app.log",
+            outputTemplate: outputTemplate,
+            rollingInterval: RollingInterval.Day,
+            flushToDiskInterval: TimeSpan.FromSeconds(1),
+            shared: true
         )
         .CreateLogger();
-
-    builder.Host.UseSerilog();
 }
 
-builder.Services.AddScoped<IClienteService, ClienteService>();
-builder.Services.AddScoped<IPropostaService, PropostaService>();
-builder.Services.AddScoped<IPropostaProcessingService, PropostaProcessingService>();
-builder.Services.AddProblemDetails();
-builder.Services.AddExceptionHandler<ClienteExceptionHandler>();
-builder.Services.AddExceptionHandler<ClienteNotFoundExceptionHandler>();
-builder.Services.AddExceptionHandler<PropostaExceptionHandler>();
-builder.Services.AddExceptionHandler<PropostaNotFoundExceptionsHandler>();
-
-builder.Services.AddCors(options =>
+// Aplica migrations
+using (var scope = app.Services.CreateScope())
 {
-    options.AddPolicy("PermissiveCors", policy =>
-    {
-        policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader();
-    });
-});
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.ConfigureHttpJsonOptions(options => {
-    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
-});
-
-builder.Services.AddHangfire(config =>
-{
-    config.UseMemoryStorage();
-});
-builder.Services.AddHangfireServer();
-
-builder.Services.AddSingleton<IMessagePublisher, RabbitMQPublisher>();
-
-var app = builder.Build();
+    var dbContext = scope.ServiceProvider.GetRequiredService<CreditScoringEngineContext>();
+    dbContext.Database.Migrate();
+}
 
 app.UseHangfireDashboard();
 
@@ -107,22 +151,16 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors("PermissiveCors");
-
 app.UseHttpsRedirection();
 app.UseExceptionHandler();
 
 app.MapClienteEndpoints();
 app.MapPropostaEndpoints();
 
-
-
-//agendamento de servico que processa as propostas pendentes!!
 RecurringJob.AddOrUpdate<IPropostaProcessingService>(
     "Processar-Propostas-Pendentes",
     service => service.ProcessarPropostasPendentesAsync(),
-    "*/2 * * * *" // todo início de hora
-);
-
+    "*/2 * * * *");
 
 app.Run();
 
